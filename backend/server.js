41eb68ca64c710ba803d21808113dc8818a702b0
aman52kwah <amankwaamichael52@gmail.com>
2025-12-01 08:03:51 +0000

    add redis for caching

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { Sequelize } from "sequelize";
 import passport from 'passport';
 import bodyParser from 'body-parser';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import {createClient} from 'redis';
 //import { sequelize, User } from './models';
 import "./models/index.js";
 //const jwt = require('jsonwebtoken');
@@ -17,6 +18,27 @@ const SequelizeStore = connectSessionSequelize(session.Store);
 const {sequelize, User, Products, OrderItems, Orders, CustomOrder} =await modelsPromise;
 
 const app= express();
+
+const client = createClient({ url: process.env.REDIS_URL});
+
+client.connect();
+
+app.get('/api/data', async (req, res) => {
+  const cacheKey = 'api:data';
+
+  let data = await client.get(cacheKey);
+  if (data) {
+    console.log('Cache hit');
+    return res.json(JSON.parse(data));
+  }
+
+  // Simulate slow API call
+  data = { message: 'Fresh data from API' };  // Replace with actual fetch
+  await client.set(cacheKey, JSON.stringify(data), { EX: 3600 });  // Expires in 60 seconds
+
+  console.log('Cache miss');
+  res.json(data);
+});
 
 app.use(cors({
     origin:process.env.CLIENT_URL || 'http://http://localhost:3000',
@@ -30,7 +52,7 @@ const urlencodedParser = bodyParser.urlencoded({
   extended: false,
 });
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
+console.log(" DB  CONNECTED SUCESSFULLY");
 
 const sessionStore = new SequelizeStore({
   db: sequelize,
@@ -133,6 +155,40 @@ passport.use(new GoogleStrategy({
   }
 }));
 
+//=====get user=====
+app.get('/api/auth/user',(req,res)=>{
+  if(req.isAuthenticated()){
+    res.json(req.user);
+  }else {
+    res.status(401).json({message:'not authenticated'})
+  }
+});
+
+
+app.post('/api/auth/logout', (req,res) =>{
+  req.logout(()=>{
+  res.json({message:'logout successfully'});
+  });
+});
+
+
+//======= middle to check authentication
+const isAuthenticated= (res,req,next)=>{
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.status(401).json({message:'unauthorized'});
+};
+
+//======middle to authenticate admin role
+const isAdmin = (req, res, next) => {
+  if (req.isAuthenticated() && req.user.role === 'admin') {
+    return next();
+  }
+  res.status(403).json({ message: 'Forbidden: Admin access required' });
+};
+
+
 
 //=================== SERIALIZATION AND DESERIALIZATION==================//
 passport.serializeUser((user,done) =>{
@@ -165,10 +221,6 @@ app.get('/api/auth/google/callback',
 )
 
 
-
-
-
-
 /*API endpoints
 POST,/auth/google,"Verify Google token, create/find user, return JWT",No
 GET,/products,List ready-made products (filter by size/material),No
@@ -182,12 +234,135 @@ GET,/payments/verify/:reference,"Verify payment, update order status",Yes */
 
 
 
+//=========== GET ALL USERS ==================
+app.get('/api/users', async( req,res) =>{
+  try {
+    const user = await User.findAll();
+    res.json(user)
+  } catch (error) {
+    console.error("users not found",error);
+    res.status(500).json({message:"internal server error"});
+  }
+});
+
+//=================== PRODUCTS ENDPOINTS ==================//
+
+// GET all products with optional filters
+app.get('/api/products',async (req,res) => {
+  try{
+    const {size,material}= req.query;
+    const where = {};
+
+    if(size) where.size = size;
+    if(material) where.material = material;
+
+    const products = await Products.findAll({where});
+    res.json(products);
+  } catch(error){
+    res.status(500).json({error:error.message});
+  }
+});
+
+// POST create product (admin only)
+app.post('/api/products', async (req,res)=>{
+  try {
+    const product = await Products.create(req.body);
+    res.status(201).json(product)
+  } catch (error) {
+    res.status(400).json({error:error.message});
+  }
+});
+
+//PUT update product (admin only)
+app.put('/api/products/:id',async (req,res) =>{
+  try {
+    const product = await Products.findByPk(req.params.id);
+    if(!product) return res.status(404).json({error:'Product found'});
+
+    await product.update(req.body);
+    res.json(product);
+  } catch (error) {
+    res.status(400).json({error:error.message});
+  }
+});
+
+//DELETE products (admin only)
+app.delete('/api/product/:id',async(req,res) =>{
+  try {
+      const product = await Products.findByPk(req.params.id);
+      if(!product) return res.status(404).json({error:'Product not found'});
+
+      await product.destroy();
+      res.json({message:'Product deleted'});
+  } catch (error) {
+    res.status(400).json({error:error.message});
+  }
+});
+
+  //=================== CUSTOM ORDERS ENDPOINTS ==================//
+  //POST create custom orders
+  app.get('/api/custom-orders', async (req,res) =>{
+    try {
+      const customOrder = await customOrder.create({
+        ...req.body,
+        userId:req.user.id,
+        paymentStatus:'pending',
+        amountDue: req.body.totalPrice * 0.5 //50% deposit
+      });
+      res.status(201).json(customOrder);
+    } catch (error) {
+      res.status(400).json({error:error.messagwe});
+    }
+  });
 
 
 
 
 
 
+
+
+
+
+//=================== ORDERS ENDPOINTS ==================//
+
+// GET user orders
+app.get('/api/orders', async (req,res) =>{
+  try {
+    const orders = await Orders.findAll({
+      where:{userId:req.id},
+      include:[OrderItems]
+    });
+    res.json(orders);
+  } catch (error) {
+    res.status(500).json({error:error.message});
+  }
+})
+
+
+  //=================== PAYMENT ENDPOINTS ==================//
+  //POST INITIALIZE PAYMENT
+  app.post('/api/payments/initialize', async (_req,res) =>{
+    try {
+      //TODO integrate with Paystack API
+
+      res.json({message:'Payment initialization endpoint '});
+      
+    } catch (error) {
+      res.status(400).json({error:error.message});
+    }
+  });
+
+  //GET VERIFY PAYMENT
+  app.get('/api/payments/verify/:reference', async (req,res) =>{
+    try {
+      //TODO: Integrate with paystack API
+      res.json({message:'Payment verification endpoint'});
+
+    } catch (error) {
+      res.status(500).json({error:error.message});
+    }
+  });
 
 
 
